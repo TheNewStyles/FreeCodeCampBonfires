92fbc5fddaa41663396096cd0b10950dad5ad590
Ty <tyler.peterson@utah.edu>
2017-07-23 10:56:54 -0600

    Create translatePigLatin.js
    Moving all Bonfire solutions into a single repo

---
+++ translatePigLatin.js
@@ -0,0 +1,23 @@
+function translatePigLatin(str) {
+  var i=0;
+  var vowelArr = [97,101,105,111,117];
+  var strBeforeVowel = '';
+  var strVowelOn = '';
+  var pigLatin = '';
+  
+  while(i<str.length){
+    if(vowelArr.includes(str.charCodeAt(i))){
+      if(i===0){
+        pigLatin = str + "way";
+        console.log("in loop "+pigLatin);
+        return pigLatin;
+      }     
+      
+      strBeforeVowel = str.substr(0,i)+"ay";
+      strVowelOn = str.substr(i, str.length);     
+      pigLatin += strVowelOn + strBeforeVowel;
+      
+      return pigLatin;
+    }    
+    i++;
+  }
