1267ed8b69d68480a8ab950b3b7d389bd01a7716
Ty <tyler.peterson@utah.edu>
2017-07-23 11:08:17 -0600

    Create README
    Moving all Bonfire solutions into a single repo

---
+++ slasherREADME.js
@@ -0,0 +1,7 @@
+slasher.js
+
+My solution for Free Code Camp Slasher Flick
+
+Return the remaining elements of an array after chopping off n elements from the head.
+
+The head means the beginning of the array, or the zeroth index.
