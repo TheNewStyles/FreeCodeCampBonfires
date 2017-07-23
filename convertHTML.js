4ee8837224904dcea3c04ae5f12a18d753dd2c9b
Ty <tyler.peterson@utah.edu>
2017-07-23 10:42:15 -0600

    Create README
    Moving all Bonfire solutions into a single repo

---
+++ convertHTML.js
@@ -0,0 +1,5 @@
+convertHTML.js
+
+My solution for Free Code Camp Convert HTML Entities
+
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
