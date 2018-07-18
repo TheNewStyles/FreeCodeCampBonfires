8701cc2fdc9ba9dda748573c79a195ba48d20b59
TheNewStyles <tyler.peterson@utah.edu>
2018-07-18 12:35:44 -0500

    Add solutions for es6 problems

---
+++ arrowFuncsParams.js
@@ -0,0 +1,21 @@
+// ES6: Write Arrow Functions with Parameters
+// Just like a normal function, you can pass arguments into arrow functions.
+
+// // doubles input value and returns it
+// const doubler = (item) => item * 2;
+// You can pass more than one argument into arrow functions as well.
+
+
+// Rewrite the myConcat function which appends contents of arr2 to arr1 so that the function uses arrow function syntax.
+
+// User did replace var keyword.
+// myConcat should be a constant variable (by using const).
+// myConcat should be a function
+// myConcat() returns the correct array
+// function keyword was not used.
+
+"use strict";
+
+const myConcat = (arr1, arr2) => arr1.concat(arr2);
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
