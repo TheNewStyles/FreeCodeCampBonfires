ff6a4e09337c9701097402d00acaa88e37b95a76
TheNewStyles <tyler.peterson@utah.edu>
2018-07-22 17:20:17 -0500

    Complete JavaScript Algorithms and Data Structures certification!

--- FunctionalProgramming/sortAnArrayAlphabeticallyUsingTheSortMethod.js
+++ FunctionalProgramming/sortAnArrayAlphabeticallyUsingTheSortMethod.js
@@ -27,3 +27,13 @@
 // alphabeticalOrder(["a", "d", "c", "a", "z", "g"]) should return ["a", "a", "c", "d", "g", "z"].
 // alphabeticalOrder(["x", "h", "a", "m", "n", "m"]) should return ["a", "h", "m", "m", "n", "x"].
 // alphabeticalOrder(["a", "a", "a", "a", "x", "t"]) should return ["a", "a", "a", "a", "t", "x"].
+
+function alphabeticalOrder(arr) {
+    // Add your code below this line
+    return arr.sort(function(a,b){
+      return a > b;
+    });
+    
+    // Add your code above this line
+  }
+  alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
